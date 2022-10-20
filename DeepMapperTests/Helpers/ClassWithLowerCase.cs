57cd420d46a9910f271afb2321f05eb2c37baced
Mohammad Tarem <Mohammad.Tarem@live.com>
2022-10-20 11:48:25 +0300

    Replaces SimpleMapper namespace with DeepMapper

--- DeepMapperTests/Helpers/ClassWithLowerCase.cs
+++ DeepMapperTests/Helpers/ClassWithLowerCase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleMapperTests.Helpers
+namespace DeepMapperTests.Helpers
 {
     public class ClassWithLowerCase
     {
