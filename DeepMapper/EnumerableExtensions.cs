b8720cb1ead6557eebcfe37f4e929dca89008e8b
Mohammad Tarem <Mohammad.Tarem@live.com>
2022-11-17 15:47:54 +0300

    Adds Extension for IEnumerables

---
+++ DeepMapper/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeepMapper.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T?> Map<T>(this IEnumerable<object?> objects)
+        {
+            if(ServiceCollectionExtensions.DeepMapper is null )
+            {
+                throw new Exception("This method is available through dependency injectiontion only.");
+            }
+            
+            foreach ( var item in objects )
+            {
+                yield return ServiceCollectionExtensions.DeepMapper!.Map<T>(item);
+            }
+            
+        }
+    }
+}
